68ecb71b1586fc383320b0a98c83074256039cb0
zhang-chenghe66 <2093629737@qq.com>
2023-09-30 22:28:44 +0800

    test

---
+++ test/test/test.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d", &a);//这里加上\n就出错了
+//	b = a / 100 + (a % 100) / 10 * 10 + (a % 100) % 10 * 100;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j, k;
+//	i = 5; j = 10; k = 1;
+//	printf("%d", k > i < j);//k>i是假，为0,0<10为真，为1，所以输出1
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j, k;
+//	i = 3; j = 2; k = 1;
+//	printf("%d", i < j == j < k);
+//	return 0;//i<j为0，j<k为0,0==0为1，所以输出1
+//}
+
+//int main()
+//{
+//	int i = 0;
+//
+//	//for (i = 0; i < 10; i++)
+//	for (i = 0; i < 10; ++i)
+//	//for (i = 0; i++ < 10; )//这个和前两个不等价
+//	{
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	scanf("%d", &j);
+//	for (i = 1; i < j; i += 2)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	switch (i % 3) 
+//	{
+//	case 0: printf("zero");
+//	case 1: printf("one");
+//	case 2: printf("two");
+//	}
+//	return 0;
+//}//输出onetwo，因为分支后面没有break
+
+int main()
+{
+	int arr[] = { 0 };
+	int a = 0;
+	for (a = 0; a <= 200; a++);
+
+
+
+	return 0;
+}
+
